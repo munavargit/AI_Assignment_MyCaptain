be777dc721e4867aae599f6c591af6f36fe9edd8
munavargit <mhmunavarhussain@gmail.com>
2024-08-14 21:41:07 +0530

    Add files via upload

---
+++ Different functions of Sets.py
@@ -0,0 +1,16 @@
+#Sets
+E = {0,2,4,6,8}
+N = {1,2,3,4,5}
+
+#Performing different set of functions such as:-union,intersection,difference and symmetric difference
+union_set = E.union(N)
+intersection_set = E.intersection(N)
+difference_set = E.difference(N)
+symmetric_difference_set = E.symmetric_difference(N)
+
+#result
+print("Union of E and N is",union_set)
+print("Intersection of E and N is",intersection_set)
+print("Difference of E and N is",difference_set)
+print("Symmetric difference of E and N is",symmetric_difference_set)
+
