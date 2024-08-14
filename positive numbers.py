91e96d3019e4e2ecfcd8640ac97159e981ddaec0
munavargit <mhmunavarhussain@gmail.com>
2024-08-14 21:19:18 +0530

    Add files via upload

---
+++ positive numbers.py
@@ -0,0 +1,11 @@
+#PRINTING POSITIVE NUMBERS FROM THE LIST 
+
+list1 = [12,-7,5,64,-14]
+positive_numbers = [num for num in list1 if num>0]
+print("Output:",positive_numbers)
+  
+list2 = [12,14,-95,3]
+positive_numbers = [num for num in list2 if num>0]
+print("Output:",positive_numbers)
+
+    
